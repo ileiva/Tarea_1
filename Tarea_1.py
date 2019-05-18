a0db2bb107198b19e8cd09919955aa773f1e5361
ileiva <50382216+ileiva@users.noreply.github.com>
2019-05-18 13:13:05 -0600

    Add files via upload

---
+++ Tarea_1.py
@@ -0,0 +1,19 @@
+import math
+
+value1 = math.pi
+value2 = "******"
+
+
+print(' %.1s' % value2 + ' %.7f' % value1
+      + ' %.2s' % value2 + ' %.6f' % value1
+      + ' %.3s' % value2 + '%.5f' % value1
+      + ' %.4s' % value2 + '%.4f' % value1
+      + ' %.5s' % value2 + '%.3f' % value1
+      + ' %.6s' % value2 + '%.2f' % value1)
+
+
+
+
+
+
+
